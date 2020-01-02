d9a10ce0996be332fa78a72ee58b65480d80f136
Zach Wahrer <zachtheclimber@gmail.com>
2020-01-02 16:19:25 -0700

    Working with floats and ints

---
+++ float-int.py
@@ -0,0 +1,26 @@
+from math import pi
+
+list = [1, 2, 3]
+print(type(list))
+
+# for i in range(10000):
+#     print(i % 500)
+
+print(round(abs(-5.4)))
+print(round((pi + 2) * 3))
+
+# Guess the output of each answer before you click RUN
+# Try to write down your answer before and see how you do...
+# keep it mind I made it a little tricky for you :)
+print((5 + 4) * 10 / 2)  # 45
+print(((5 + 4) * 10) / 2)  # 45
+print((5 + 4) * (10 / 2))  # 45
+print(5 + (4 * 10) / 2)  # 25
+print(5 + 4 * 10 // 2)  # 25
+
+print(bin(35))
+print(int('0b10010010', 2))
+
+a, b, c = 1, 2, 3
+
+print(a + b + c)
