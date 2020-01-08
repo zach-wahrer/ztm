c27eaaaa45714d81e034755bc46e59a326b3a6e0
Zach Wahrer <zachtheclimber@gmail.com>
2020-01-08 14:22:13 -0700

    Add list slicing exercise

---
+++ list-slicing.py
@@ -0,0 +1,14 @@
+# What is the output of this code?
+# Before you clikc RUN, guess the output of each print statement!
+new_list = ['a', 'b', 'c']
+print(new_list[1])  # b
+print(new_list[-2])  # b
+print(new_list[1:3])  # b c
+new_list[0] = 'z'
+print(new_list)  # z b c
+
+my_list = [1, 2, 3]
+bonus = my_list + [5]
+my_list[0] = 'z'
+print(my_list)  # z 2 3
+print(bonus)  # 1 2 3 5
