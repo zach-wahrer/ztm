a7b37d90dcb28ce81bbd8bc519c9d599c0bb3d5e
Zach Wahrer <zachtheclimber@gmail.com>
2020-01-08 15:16:12 -0700

    Add additional exercise

--- list-methods.py
+++ list-methods.py
@@ -14,3 +14,31 @@ print(basket.count('Apples'))
 # 6. empty the basket
 basket.clear()
 print(basket)
+
+
+# fix this code so that it prints a sorted list of all of our friends (alphabetical). Scroll to see answer
+friends = ['Simon', 'Patty', 'Joy', 'Carrie', 'Amira', 'Chu']
+
+new_friend = ['Stanley']
+
+print(sorted(friends + new_friend))
+
+
+friends = ['Simon', 'Patty', 'Joy', 'Carrie', 'Amira', 'Chu']
+
+new_friend = ['Stanley']
+
+all_friends = friends + new_friend
+all_friends.sort()
+
+print(all_friends)
+
+
+friends = ['Simon', 'Patty', 'Joy', 'Carrie', 'Amira', 'Chu']
+
+new_friend = ['Stanley']
+
+friends.extend(new_friend)
+friends.sort()
+
+print(friends)
