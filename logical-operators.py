383f9849eda142be73b3d327873296add71b04ab
Zach Wahrer <zachtheclimber@gmail.com>
2020-01-20 11:13:37 -0700

    Add logical operators exercise

---
+++ logical-operators.py
@@ -0,0 +1,9 @@
+is_magician = True
+is_expert = False
+
+if is_magician and is_expert:
+    print('You are a master magician.')
+elif is_magician and not is_expert:
+    print("At least you're getting there.")
+elif not is_magician:
+    print("You need magic powers.")
