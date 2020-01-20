caca725a5d7446e2443b90c0839269a04b67e9ed
Zach Wahrer <zachtheclimber@gmail.com>
2020-01-20 12:48:15 -0700

    Add basic GUI exercise

---
+++ basic_gui.py
@@ -0,0 +1,16 @@
+picture = [
+    [0, 0, 0, 1, 0, 0, 0],
+    [0, 0, 1, 1, 1, 0, 0],
+    [0, 1, 1, 1, 1, 1, 0],
+    [1, 1, 1, 1, 1, 1, 1],
+    [0, 0, 0, 1, 0, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0]
+]
+
+fill, empty = '8', ' '
+
+for row in picture:
+    for input in row:
+        char = fill if input else empty
+        print(char, end="")
+    print('')
