c702f7748a34b95f4de18ea5d6e78212134a1701
Zach Wahrer <zachtheclimber@gmail.com>
2020-01-23 09:50:17 -0700

    Add decorator practice

---
+++ decorator.py
@@ -0,0 +1,15 @@
+def my_decorator(func):
+    def wrap_func(*args, **kwargs):
+        print('***')
+        func(*args, **kwargs)
+        print('***')
+    return wrap_func
+
+
+@my_decorator
+def hello(greeting='hello', emoji=':-|'):
+    print(greeting, emoji)
+
+
+hello('hiii', ':-)')
+hello()
