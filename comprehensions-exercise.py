10ad6811b7463663dfc0c2f235c99ee94def4c7f
Zach Wahrer <zachtheclimber@gmail.com>
2020-01-22 22:41:57 -0700

    Add list comprehension exercise

---
+++ comprehensions-exercise.py
@@ -0,0 +1,12 @@
+some_list = ['a', 'b', 'c', 'b', 'd', 'm', 'n', 'n']
+
+duplicates = []
+for value in some_list:
+    if some_list.count(value) > 1:
+        if value not in duplicates:
+            duplicates.append(value)
+
+print(duplicates)
+
+duplicates_comp = list({x for x in some_list if some_list.count(x) > 1})
+print(duplicates_comp)
