f49ac327fafaf53a516b60a83ff600978e577a5c
Zach Wahrer <zachtheclimber@gmail.com>
2020-01-25 12:48:39 -0700

    Add performance checking for has-pair-with-sum

--- dsa/has-pair-with-sum.py
+++ dsa/has-pair-with-sum.py
@@ -1,3 +1,6 @@
+from helpers.performance import run_time
+import random
+
 '''
 Collection of numbers, and a sum
 
@@ -6,10 +9,15 @@ Collection of numbers, and a sum
 
 
 '''
-input_list = [1, 2, 4, 3, 'a', 5]
-input_sum = 9
+# input_list = [1, 2, 4, 3, 'a', 5]
+# input_sum = 9
+
+input_list = [i for i in range(500000)]
+random.shuffle(input_list)
+input_sum = 1000
 
 
+@run_time
 # Brute Force - O(n^2)
 def has_pair_with_sum_brute(nums: list, input_sum: int) -> bool:
     # for loop over nums
@@ -24,40 +32,39 @@ def has_pair_with_sum_brute(nums: list, input_sum: int) -> bool:
     return False
 
 
-# print(has_pair_with_sum_brute(input_list, input_sum))
+print(has_pair_with_sum_brute(input_list, input_sum))
 
 
+@run_time
 # O(n log n)
 def has_pair_with_sum_n_log_n(nums: list, input_sum: int) -> bool:
     # Establish pointer that starts at beginning
     # For loop that goes over the rest of the list, checking for sum
     # Each time the loop runs, increment the pointer posistion
 
-    pointer = 0
+    pointer1 = 0
     list_length = len(input_list) - 1
-    while pointer < list_length:
-        if isinstance(nums[pointer], int):
-            for i in nums[pointer + 1:]:
-                if isinstance(i, int):
-                    if nums[pointer] + i == input_sum:
-                        return True
-        pointer += 1
+    while pointer1 < list_length:
+        for pointer2 in nums[pointer1 + 1:]:
+            if nums[pointer1] + pointer2 == input_sum:
+                return True
+        pointer1 += 1
     return False
 
 
-# print(has_pair_with_sum_n_log_n(input_list, input_sum))
+print(has_pair_with_sum_n_log_n(input_list, input_sum))
 
 
+@run_time
 # O(n)
 def has_pair_with_sum_n(nums, input_sum):
     # Loop through nums, adding compliment if no match found in set
     compliments = set()
     for number in nums:
-        if isinstance(number, int):
-            if number in compliments:
-                return True
-            else:
-                compliments.add(input_sum - number)
+        if number in compliments:
+            return True
+        else:
+            compliments.add(input_sum - number)
     return False
 
 
