76cc0c18c56d9c0d0eccbdfa32d8cb33643f2fd3
Zach Wahrer <zachtheclimber@gmail.com>
2020-01-26 07:43:33 -0700

    Add O(n1 + n2) solution for merge_sorted_arrays

--- dsa/exercises/merge_sorted_arrays.py
+++ dsa/exercises/merge_sorted_arrays.py
@@ -1,7 +1,11 @@
-array1 = [0, 3, 4, 31]
-array2 = [4, 6, 30]
+from helpers.performance import run_time
+# array1 = [0, 3, 4, 31, 35, 36, 49, 49, 49, 49]
+# array2 = [4, 6, 30, 32, 38, 50, 50]
+array1 = [i for i in range(10000000)]
+array2 = [i for i in range(11000000)]
 
 
+@run_time
 # Brute Force - O(nlog(n))
 def merge_sorted_arrays_brute(array1, array2):
     merged_array = array1 + array2
@@ -9,7 +13,7 @@ def merge_sorted_arrays_brute(array1, array2):
     return merged_array
 
 
-# print(merge_sorted_arrays_brute(array1, array2))
+merge_sorted_arrays_brute(array1, array2)
 
 
 # Non-functional solution
@@ -28,25 +32,37 @@ def merge_sorted_arrays_brute(array1, array2):
 
 # print(merge_sorted_arrays_zip(array1, array2))
 
-
-# Two pointers O(a+b)
+@run_time
+# Two pointers O(n1+n2)
 def merge_sorted_arrays3(array1, array2):
     pointer1 = 0
     pointer2 = 0
-    merged_array = []
-    while True:
-        if pointer1 <= len(array1) - 1:  # 3
-            while array1[pointer1] <= array2[pointer2]:
-                merged_array.append(array1[pointer1])
-                pointer1 += 1
-                print(merged_array)
-        if pointer2 <= len(array2) - 1:  # 2
-            while array2[pointer2] <= array1[pointer1]:
-                merged_array.append(array2[pointer2])
-                pointer2 += 1
-                print(merged_array)
+    merge_pointer = 0
+    length1 = len(array1)
+    length2 = len(array2)
+    merged_array = [None] * (length1 + length2)
+    while pointer1 < length1 and pointer2 < length2:
+
+        if array1[pointer1] < array2[pointer2]:
+            merged_array[merge_pointer] = array1[pointer1]
+            pointer1 += 1
+            merge_pointer += 1
+        else:
+            merged_array[merge_pointer] = array2[pointer2]
+            pointer2 += 1
+            merge_pointer += 1
+
+    while pointer1 < length1:
+        merged_array[merge_pointer] = array1[pointer1]
+        pointer1 += 1
+        merge_pointer += 1
+
+    while pointer2 < length2:
+        merged_array[merge_pointer] = array2[pointer2]
+        pointer2 += 1
+        merge_pointer += 1
 
     return merged_array
 
 
-print(merge_sorted_arrays3(array1, array2))
+merge_sorted_arrays3(array1, array2)
